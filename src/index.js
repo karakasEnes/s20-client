16616e2668073458c6d54957673b2ddffb9357ff
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-19 14:06:57 +0300

    redux setup, actions integrate, google auth connect with redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import allReducer from "./reducers";
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const store = createStore(allReducer);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.querySelector("#root")
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+  isSignIn: () => 2311,
+});
