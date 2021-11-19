a425d2598ca69849a8a9d273f3e2bbc2ecbd6410
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-19 11:29:06 +0300

    add google button

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -25,11 +25,21 @@ class GoogleAuth extends React.Component {
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>We don't know about signed in or out</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in </div>;
+      return (
+        <button className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>I am not signed in</div>;
+      return (
+        <button className="ui red google button">
+          <i className="google icon" />
+          Sign In With Google
+        </button>
+      );
     }
   }
 
