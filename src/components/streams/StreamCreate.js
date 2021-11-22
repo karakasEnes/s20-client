05377ea4c572989f6df0235b81c171435e42f10c
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-22 13:32:51 +0300

    display validation

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 class StreamCreate extends React.Component {
-  renderInput({ input, label }) {
+  renderInput({ input, label, meta }) {
     return (
       <div className="field">
         <label>{label}</label>
         <input {...input} />
+        <div>{meta.error}</div>
       </div>
     );
   }
@@ -32,6 +33,21 @@ class StreamCreate extends React.Component {
   }
 }
 
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter description";
+  }
+
+  return errors;
+};
+
 export default reduxForm({
   form: "streamCreate",
+  validate,
 })(StreamCreate);
