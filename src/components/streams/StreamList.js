3e573c7bab3a8f2da4732bef8527572ef6abc410
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-23 11:47:20 +0300

    render streamList, createStream with userId

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,9 +7,32 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderList = () => {
+    return this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  };
+
   render() {
-    return <div>StreamList</div>;
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui called list">{this.renderList()}</div>
+      </div>
+    );
   }
 }
 
-export default connect(null, { fetchStreams })(StreamList);
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams) };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
