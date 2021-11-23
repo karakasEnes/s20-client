ce3e72883653a6f3d00b43037650546e8c6ddbdf
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-23 16:23:36 +0300

    programmatically navigate, our own history add to project

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import streams from "../apis/streams";
+import streams from '../apis/streams';
+import history from '../history';
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -7,7 +8,7 @@ import {
   FETCH_STREAM,
   EDIT_STREAM,
   DELETE_STREAM,
-} from "./types";
+} from './types';
 
 export const signIn = (userId) => {
   return { type: SIGN_IN, payload: userId };
@@ -19,12 +20,13 @@ export const signOut = () => {
 
 export const createStream = (formValues) => async (dispatch, getState) => {
   const { userId } = getState().auth;
-  const response = await streams.post("streams", { ...formValues, userId });
+  const response = await streams.post('streams', { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const fetchStreams = () => async (dispatch) => {
-  const response = await streams.get("/streams");
+  const response = await streams.get('/streams');
   dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
