1c80eb130cfbce43c3081f24700092bb05a5550c
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-24 15:39:04 +0300

    reusable Modal, we have small issue about gap, because of having extra div element for actions component

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -2,10 +2,21 @@ import React from 'react';
 import Modal from '../Modal';
 
 const StreamDelete = () => {
+  const actions = (
+    <div>
+      <button className='ui primary button'>Delete</button>
+      <button className='ui button'>Cancel</button>
+    </div>
+  );
+
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title='Delete Stream'
+        content='Are you sure you want to remove this Stream!?'
+        actions={actions}
+      />
     </div>
   );
 };
