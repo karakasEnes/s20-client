c822310ad2a3d345144326e1af13436674b4b734
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-25 11:43:38 +0300

    component unmount functionality add

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -11,6 +11,24 @@ class StreamShow extends React.Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    console.log('component unmount.');
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
     this.player = flv.createPlayer({
       type: 'flv',
       url: `http://localhost:8000/live/${id}.flv`,
@@ -18,7 +36,6 @@ class StreamShow extends React.Component {
 
     this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
-    this.player.play();
   }
 
   render() {
